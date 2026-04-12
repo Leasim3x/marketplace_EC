8fc1600fe2364d775db825b165ea7eab1f3d54ec
Misael Manzanales Moreno <leasim3x@gmail.com>
2026-04-11 23:44:43 -0600

    feat(products): aplicación de la lógica y puntos de acceso de la API para el módulo de productos

--- src/modules/productos/dto/actualizar-stock.dto.ts
+++ src/modules/productos/dto/actualizar-stock.dto.ts
@@ -1,7 +1,9 @@
+import { Type } from 'class-transformer';
 import { IsNumber, Min } from 'class-validator';
 
 export class ActualizarStockDto {
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0)
   stock: number;
--- src/modules/productos/productos.controller.ts
+++ src/modules/productos/productos.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+
+import { ProductosService } from './productos.service';
+import { CrearProductoDto } from './dto/crear-producto.dto';
+import { ActualizarProductoDto } from './dto/actualizar-producto.dto';
+import { ActualizarStockDto } from './dto/actualizar-stock.dto';
 
 @Controller('productos')
-export class ProductosController {}
+export class ProductosController {
+
+    constructor(readonly productosService: ProductosService) { }
+
+    @Post()
+    crearProducto(@Body() data: CrearProductoDto) {
+
+        return this.productosService.crearProducto(data);
+    }
+
+    @Get()
+    listarProductos() {
+
+        return this.productosService.listarTodosProductos();
+
+    }
+
+    @Get('activos')
+    listarProductosActivos() {
+
+        return this.productosService.listarActivos();
+
+    }
+
+    @Get('desactivados')
+    listarProductosDesactivados() {
+
+        return this.productosService.listarDesactivados();
+        
+    }
+
+    @Get(':id')
+    obtenerProducto(@Param('id') id: number) {
+
+        return this.productosService.obtenerProducto(id);
+
+    }
+
+    @Patch(':id')
+    actualizarProducto(@Param('id') id: number, @Body() data: ActualizarProductoDto) {
+
+        return this.productosService.actualizarProducto(id, data);
+
+    }
+
+    @Patch(':id/stock')
+    actualizarStock(@Param('id') id: number, @Body() data: ActualizarStockDto) {
+
+        return this.productosService.actualizarStock(id, data);
+
+    }
+
+    @Patch(':id/activar')
+    activarProducto(@Param('id') id: number) {
+
+        return this.productosService.activarProducto(id);
+
+    }
+
+    @Patch(':id/desactivar')
+    desactivarProducto(@Param('id') id: number) {
+
+        return this.productosService.desactivarProducto(id);
+
+    }
+}
